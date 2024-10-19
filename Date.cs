70e4ad84ea6a56b3eb85fb14b1e338b04ce54b37
nordinzakaria <nordinzakaria@gmail.com>
2024-10-19 22:35:07 +0800

    Initial commit

---
+++ Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Date
+{
+    private int day;
+    private int month;
+    private int year;
+
+    public Date() { }
+    public Date(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int GetDay() { return day; }
+    public int GetMonth() { return month; }
+    public int GetYear() { return year; }
+    public void SetDay(int day) { this.day = day; }
+    public void SetMonth(int month) { this.month = month; }
+    public void SetYear(int year) { this.year = year; }
+}
---
+++ Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Transaction
+{
+    private int val;
+    private Date date;
+
+    public Transaction() { }
+    public Transaction(int val, Date date)
+    {
+        this.val = val;
+        this.date = date;
+    }
+    
+    public void SetVal(int val) { this.val = val; }
+    public void SetDate(Date date) { this.date = date; }
+    public int GetVal() { return val; }
+    public Date GetDate() { return date; }
+
+}
